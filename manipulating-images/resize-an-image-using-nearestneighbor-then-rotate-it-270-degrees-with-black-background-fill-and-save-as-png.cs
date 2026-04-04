30f50a3b3409185f23db83701d3e6ec5b9ddae26
agent-aspose-imaging-examples <agentic.imaging@aspose.com>
2026-04-04 20:23:57 +0500

    feat: add example resize-an-image-using-nearestneighbor-then-rotate-it-270-degrees-with-black-background-fill-and-save-as-png

---
+++ manipulating-images/resize-an-image-using-nearestneighbor-then-rotate-it-270-degrees-with-black-background-fill-and-save-as-png.cs
@@ -0,0 +1,29 @@
+using System;
+using System.IO;
+using Aspose.Imaging;
+using Aspose.Imaging.ImageOptions;
+
+class Program
+{
+    static void Main()
+    {
+        string inputPath = "input.jpg";
+        string outputPath = "output.png";
+
+        if (!File.Exists(inputPath))
+        {
+            Console.Error.WriteLine($"File not found: {inputPath}");
+            return;
+        }
+
+        Directory.CreateDirectory(Path.GetDirectoryName(outputPath));
+
+        using (RasterImage image = (RasterImage)Image.Load(inputPath))
+        {
+            image.Resize(image.Width / 2, image.Height / 2, ResizeType.NearestNeighbourResample);
+            image.Rotate(270f, true, Color.Black);
+            var pngOptions = new PngOptions();
+            image.Save(outputPath, pngOptions);
+        }
+    }
+}
