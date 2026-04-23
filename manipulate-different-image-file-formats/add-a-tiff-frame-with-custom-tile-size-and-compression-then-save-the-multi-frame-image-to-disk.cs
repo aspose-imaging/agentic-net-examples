8bccc0d772636f74041cb5de9ba02c1082ba71f1
agent-aspose-imaging-examples <agentic.imaging@aspose.com>
2026-04-23 18:03:59 +0500

    feat: add example add-a-tiff-frame-with-custom-tile-size-and-compression-then-save-the-multi-frame-image-to-disk

--- manipulate-different-image-file-formats/add-a-tiff-frame-with-custom-tile-size-and-compression-then-save-the-multi-frame-image-to-disk.cs
+++ manipulate-different-image-file-formats/add-a-tiff-frame-with-custom-tile-size-and-compression-then-save-the-multi-frame-image-to-disk.cs
@@ -5,6 +5,7 @@ using Aspose.Imaging.ImageOptions;
 using Aspose.Imaging.FileFormats.Tiff;
 using Aspose.Imaging.FileFormats.Tiff.Enums;
 using Aspose.Imaging.Brushes;
+using Aspose.Imaging;
 
 class Program
 {
@@ -12,46 +13,54 @@ class Program
     {
         try
         {
-            string outputPath = "output_multi.tif";
+            string outputPath = "C:\\temp\\multiframe.tif";
 
-            // Ensure the output directory exists
             Directory.CreateDirectory(Path.GetDirectoryName(outputPath));
 
-            // First frame with default LZW compression
+            // Options for the first frame
             TiffOptions options1 = new TiffOptions(TiffExpectedFormat.Default);
             options1.BitsPerSample = new ushort[] { 8, 8, 8 };
             options1.Compression = TiffCompressions.Lzw;
             options1.Photometric = TiffPhotometrics.Rgb;
             options1.PlanarConfiguration = TiffPlanarConfigs.Contiguous;
 
-            TiffFrame frame1 = new TiffFrame(options1, 200, 200);
-            LinearGradientBrush brush1 = new LinearGradientBrush(
-                new Point(0, 0),
-                new Point(frame1.Width, frame1.Height),
-                Color.Blue,
-                Color.Yellow);
-            Graphics graphics1 = new Graphics(frame1);
-            graphics1.FillRectangle(brush1, frame1.Bounds);
-
-            // Second frame with Deflate compression
-            TiffOptions options2 = new TiffOptions(TiffExpectedFormat.Default);
-            options2.BitsPerSample = new ushort[] { 8, 8, 8 };
-            options2.Compression = TiffCompressions.Deflate;
-            options2.Photometric = TiffPhotometrics.Rgb;
-            options2.PlanarConfiguration = TiffPlanarConfigs.Contiguous;
-
-            TiffFrame frame2 = new TiffFrame(options2, 200, 200);
-            LinearGradientBrush brush2 = new LinearGradientBrush(
-                new Point(0, 0),
-                new Point(frame2.Width, frame2.Height),
-                Color.Green,
-                Color.Red);
-            Graphics graphics2 = new Graphics(frame2);
-            graphics2.FillRectangle(brush2, frame2.Bounds);
-
-            // Create a multi‑frame TIFF image and save it
-            using (TiffImage tiffImage = new TiffImage(new TiffFrame[] { frame1, frame2 }))
+            // Create TIFF image with first frame
+            using (TiffImage tiffImage = (TiffImage)Image.Create(options1, 200, 200))
             {
+                // Draw gradient on first frame
+                Graphics graphics1 = new Graphics(tiffImage);
+                using (LinearGradientBrush brush1 = new LinearGradientBrush(
+                    new Point(0, 0),
+                    new Point(200, 200),
+                    Color.Blue,
+                    Color.Yellow))
+                {
+                    graphics1.FillRectangle(brush1, tiffImage.ActiveFrame.Bounds);
+                }
+
+                // Options for the second frame
+                TiffOptions options2 = new TiffOptions(TiffExpectedFormat.Default);
+                options2.BitsPerSample = new ushort[] { 8, 8, 8 };
+                options2.Compression = TiffCompressions.Deflate;
+                options2.Photometric = TiffPhotometrics.Rgb;
+                options2.PlanarConfiguration = TiffPlanarConfigs.Contiguous;
+
+                // Create second frame and add to image
+                TiffFrame frame2 = new TiffFrame(options2, 200, 200);
+                tiffImage.AddFrame(frame2);
+                tiffImage.ActiveFrame = frame2;
+
+                // Draw gradient on second frame
+                Graphics graphics2 = new Graphics(tiffImage);
+                using (LinearGradientBrush brush2 = new LinearGradientBrush(
+                    new Point(0, 0),
+                    new Point(200, 200),
+                    Color.Red,
+                    Color.Green))
+                {
+                    graphics2.FillRectangle(brush2, tiffImage.ActiveFrame.Bounds);
+                }
+
                 tiffImage.Save(outputPath);
             }
         }
