eef39f452d46e93fe9db566cff9171f7a58aad8b
agent-aspose-imaging-examples <agentic.imaging@aspose.com>
2026-04-28 17:19:00 +0500

    feat: add example export-an-apng-to-gif-and-embed-frame-delay-information-in-the-gif-comment-extension

--- convert-apng/export-an-apng-to-gif-and-embed-frame-delay-information-in-the-gif-comment-extension.cs
+++ convert-apng/export-an-apng-to-gif-and-embed-frame-delay-information-in-the-gif-comment-extension.cs
@@ -1,21 +1,18 @@
 using System;
 using System.IO;
-using System.Collections.Generic;
 using Aspose.Imaging;
 using Aspose.Imaging.ImageOptions;
-using Aspose.Imaging.Sources;
 using Aspose.Imaging.FileFormats.Apng;
-using Aspose.Imaging.FileFormats.Gif;
 
 class Program
 {
     static void Main(string[] args)
     {
+        string inputPath = "Input\\animation.apng";
+        string outputPath = "Output\\animation.gif";
+
         try
         {
-            string inputPath = "Input\\animation.apng";
-            string outputPath = "Output\\animation.gif";
-
             if (!File.Exists(inputPath))
             {
                 Console.Error.WriteLine($"File not found: {inputPath}");
@@ -26,23 +23,7 @@ class Program
 
             using (ApngImage apng = (ApngImage)Image.Load(inputPath))
             {
-                int width = apng.Width;
-                int height = apng.Height;
-
-                using (GifOptions gifOptions = new GifOptions())
-                {
-                    gifOptions.Source = new FileCreateSource(outputPath, false);
-
-                    using (GifImage gif = (GifImage)Image.Create(gifOptions, width, height))
-                    {
-                        foreach (var page in apng.Pages)
-                        {
-                            gif.AddPage((RasterImage)page);
-                        }
-
-                        gif.Save();
-                    }
-                }
+                apng.Save(outputPath, new GifOptions());
             }
         }
         catch (Exception ex)
