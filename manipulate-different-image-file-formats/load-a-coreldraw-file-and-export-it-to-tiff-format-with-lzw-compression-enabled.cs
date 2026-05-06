d17c8baf01d4737a575406aceb1c7add8894c74d
agent-aspose-imaging-examples <agentic.imaging@aspose.com>
2026-05-06 18:05:15 +0500

    feat: add example load-a-coreldraw-file-and-export-it-to-tiff-format-with-lzw-compression-enabled

--- manipulate-different-image-file-formats/load-a-coreldraw-file-and-export-it-to-tiff-format-with-lzw-compression-enabled.cs
+++ manipulate-different-image-file-formats/load-a-coreldraw-file-and-export-it-to-tiff-format-with-lzw-compression-enabled.cs
@@ -2,8 +2,8 @@ using System;
 using System.IO;
 using Aspose.Imaging;
 using Aspose.Imaging.ImageOptions;
-using Aspose.Imaging.FileFormats.Tiff.Enums;
 using Aspose.Imaging.FileFormats.Cdr;
+using Aspose.Imaging.FileFormats.Tiff.Enums;
 
 class Program
 {
@@ -11,8 +11,8 @@ class Program
     {
         try
         {
-            string inputPath = @"C:\input\sample.cdr";
-            string outputPath = @"C:\output\sample.tif";
+            string inputPath = "C:\\input\\sample.cdr";
+            string outputPath = "C:\\output\\sample.tif";
 
             if (!File.Exists(inputPath))
             {
@@ -24,17 +24,17 @@ class Program
 
             using (CdrImage cdr = (CdrImage)Image.Load(inputPath))
             {
-                TiffOptions tiffOptions = new TiffOptions(TiffExpectedFormat.TiffLzwRgb)
+                TiffOptions tiffOptions = new TiffOptions(TiffExpectedFormat.Default);
+                tiffOptions.Compression = TiffCompressions.Lzw;
+                tiffOptions.Photometric = TiffPhotometrics.Rgb;
+                tiffOptions.BitsPerSample = new ushort[] { 8, 8, 8 };
+                tiffOptions.PlanarConfiguration = TiffPlanarConfigs.Contiguous;
+                tiffOptions.VectorRasterizationOptions = new VectorRasterizationOptions
                 {
-                    Compression = TiffCompressions.Lzw,
-                    BitsPerSample = new ushort[] { 8, 8, 8 },
-                    Photometric = TiffPhotometrics.Rgb,
-                    PlanarConfiguration = TiffPlanarConfigs.Contiguous,
-                    VectorRasterizationOptions = new CdrRasterizationOptions
-                    {
-                        PageWidth = cdr.Width,
-                        PageHeight = cdr.Height
-                    }
+                    PageWidth = cdr.Width,
+                    PageHeight = cdr.Height,
+                    TextRenderingHint = TextRenderingHint.SingleBitPerPixel,
+                    SmoothingMode = SmoothingMode.None
                 };
 
                 cdr.Save(outputPath, tiffOptions);
